c46a3b8045ae973f43dcf27433d8358fa128563a
cristian <cristian0402218@gmail.com>
2024-01-03 22:54:37 -0600

    :tada: Finish config

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,31 +1,52 @@
 return {
-  {
-  "williamboman/mason.nvim",
-  config = function()
-   require("mason").setup()
-  end
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-  config = function()
-   require("mason-lspconfig").setup({
-        ensure_installed = {"lua_ls","jdtls","tsserver","tailwindcss","html" }
-      })
-  end
-  },
-  {
-    "neovim/nvim-lspconfig",
-  config = function()
-    local lspconfig = require('lspconfig')
-    lspconfig.tsserver.setup ({})
-    lspconfig.jdtls.setup ({})
-    lspconfig.tailwindcss.setup ({})
-    lspconfig.html.setup({})
-    lspconfig.lua_ls.setup({})
-    vim.keymap.set('n','K', vim.lsp.buf.hover, {})
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-    vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
-  end
-  }
-}
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = { "lua_ls", "jdtls", "tsserver", "tailwindcss", "html" },
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		lazy = false,
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			local lspconfig = require("lspconfig")
 
+			local on_attach = function(client, bufnr)
+				require("tailwindcss-colors").buf_attach(bufnr)
+			end
+
+			lspconfig["tailwindcss"].setup({
+				on_attach = on_attach,
+			})
+
+			lspconfig.tsserver.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.jdtls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.tailwindcss.setup({
+				on_attach = on_attach,
+				capabilities = capabilities,
+			})
+			lspconfig.html.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
+			})
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+			vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, opts)
+		end,
+	},
+}
