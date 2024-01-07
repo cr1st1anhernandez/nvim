da3ed7a111a5ca6665d6b77b413478c1fed687b8
cristian <cristian0402218@gmail.com>
2024-01-07 12:13:05 -0600

    :art: Change color theme

--- lua/plugins/tokyodark.lua
+++ lua/plugins/tokyodark.lua
@@ -1,15 +1,6 @@
 return {
-	"folke/tokyonight.nvim",
-	lazy = false,
-	priority = 1000,
-	opts = {},
-	config = function()
-		require("tokyonight").setup({
-			style = "night",
-			on_colors = function(colors)
-        colors.bg = "#16161e"
-			end,
-		})
-		vim.cmd([[colorscheme tokyonight-night]])
-	end,
+  "tiagovla/tokyodark.nvim",
+  config = function()
+    vim.cmd([[colorscheme tokyodark]])
+  end,
 }
