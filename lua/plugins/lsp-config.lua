d95047563ee9e928cfa38e1dc38e881391ecb3d6
cristian <cristian0402218@gmail.com>
2024-01-11 08:53:47 -0600

    :recycle: Remove Java from LSP configuration

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -19,7 +19,6 @@ return {
 		"neovim/nvim-lspconfig",
 		lazy = false,
 		config = function()
-			require("java").setup()
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 			local lspconfig = require("lspconfig")
 
