85c8b2abb23db56aace5a816f73478548fd71a93
cristian <cristian0402218@gmail.com>
2024-01-16 22:51:18 -0600

    :heavy_plus_sign: Add Lombok support

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -3,7 +3,7 @@ return {
 		"williamboman/mason.nvim",
 		lazy = false,
 		config = function()
-			require("mason").setup()
+			require("mason").setup({})
 		end,
 	},
 	{
@@ -27,6 +27,10 @@ return {
 			})
 			lspconfig.jdtls.setup({
 				capabilities = capabilities,
+				cmd = {
+					"jdtls",
+					"--jvm-arg=" .. string.format("-javaagent:%s", vim.fn.expand("$MASON/share/jdtls/lombok.jar")),
+				},
 			})
 			lspconfig.tailwindcss.setup({
 				capabilities = capabilities,
