9b2e6371eabd42fadc927e56547a379714476b09
cristian <cristian0402218@gmail.com>
2024-01-19 14:30:24 -0600

    :lipstick: Reduce time and change style of animation

--- lua/plugins/notify.lua
+++ lua/plugins/notify.lua
@@ -2,5 +2,9 @@ return {
   "rcarriga/nvim-notify",
   config = function()
     vim.notify = require("notify")
+    vim.notify.setup({
+      stages = "slide",
+      timeout = 2000,
+    })
   end,
 }
